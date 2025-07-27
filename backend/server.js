4f19f4a399205417f11e63151ea658123c0ab3f1
Faisal <faisalkittur689@gmail.com>
2025-07-27 12:29:23 +0530

    add image upload feature

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./mongo');
-const profileRoutes = require('./profileRoutes');
+const profileRoutes = require('./src/profileRoutes');
+const cors = require('cors');
+const productRoutes = require('./src/productRoutes');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', profileRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
